9529241ba034a5ed7922ec02d2f65606c61c98b1
lhb <liuhaibo_ljf@163.com>
2018-04-23 12:31:47 +0800

    update xiao cheng xu

--- 02DevelopmentArea/04编码实现/wechatapp/pages/home/home.js
+++ 02DevelopmentArea/04编码实现/wechatapp/pages/home/home.js
@@ -8,7 +8,28 @@ Page({
             'http://img.taopic.com/uploads/allimg/140729/240450-140HZP45790.jpg',
             'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
             'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-        ]
+        ],
+        foods:[]
+    },
+    onLoad: function () {
+        // 页面渲染后 执行
+        wx.request({
+            url: 'http://localhost:8089/food?restaurant=&name=&pageNum=1&pageSize=10&total=0',
+            data: {
+               
+            },
+            // header: {
+            //     'content-type': 'application/json' // 默认值
+            // },
+            success:(res)=>{
+                console.log(res.data.list)
+                this.setData({ foods: res.data.list })
+            }
+        })
+    },
+    addFood: function (event){
+        console.log(event.target.dataset.id)
+        
     },
     toDetail:function(){
         wx.redirectTo({
